0e7ea26814883676508d8ec88977ffdcf66ca768
Ivan <imoroz404@gmail.com>
2025-01-22 20:32:16 +0300

    try push to server

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const browserSync = require('browser-sync');
 const sass = require('gulp-sass')(require('sass'));
 const cleanCSS = require('gulp-clean-css');
 const rename = require("gulp-rename");
+const autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('server', function () {
 
@@ -19,6 +20,7 @@ gulp.task('styles', function () {
     return gulp.src("src/sass/**/*.+(scss|sass)")
         .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
         .pipe(rename({ suffix: '.min', prefix: '' }))
+        .pipe(autoprefixer())
         .pipe(cleanCSS({ compatibility: 'ie8' }))
         .pipe(gulp.dest("src/css"))
         .pipe(browserSync.stream())
@@ -31,6 +33,7 @@ gulp.task('styles', function () {
 
 gulp.task('watch', function () {
     gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel('styles'));
+    gulp.watch("src/*.html").on("change", browserSync.reload)
 })
 
 gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
